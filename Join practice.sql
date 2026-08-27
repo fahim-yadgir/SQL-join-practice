3aa783f80b9b4fe9de506632263f97437797135c
Fahim <fahimyadgir07@gmail.com>
2026-08-27 14:56:23 +0530

    Perform more query and update previus query

--- Join practice.sql
+++ Join practice.sql
@@ -83,3 +83,10 @@ select customer_name , count(*)as total_count
 from customers
 group by customer_name
 order by total_count desc;
+
+
+select o.customer_id ,o.product ,c.customer_name, sum(o.amount)as total_amount
+from orders o
+left join customers c on c.customer_id = o.customer_id
+where o.customer_id = 34
+group by o.customer_id,o.product,c.customer_name;
