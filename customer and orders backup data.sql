7d3420cfdb276ed81db381d7cd7e281f58431827
Fahim <fahimyadgir07@gmail.com>
2026-08-29 11:33:38 +0530

    Perform some query on data backup

--- customer and orders backup data.sql
+++ customer and orders backup data.sql
@@ -1,5 +1,9 @@
 create table backup_customers_data
 select * from customers;
 
+drop table backup_customers_data;
+
 create table backup_orders_data
 select * from orders;
+
+select * from backup_customers_data;
